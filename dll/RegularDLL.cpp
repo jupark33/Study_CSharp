a126cbec6dafeae14d672eaeb19f0b57af5a5156
jupark33 <jupark33@gmail.com>
2021-03-23 09:45:28 +0900

    Create RegularDLL.cpp

---
+++ dll/RegularDLL.cpp
@@ -0,0 +1,19 @@
+'''
+int n = 0;
+CString s = CString(_T("hello dll"));
+
+extern "C" __declspec(dllexport) int GetInt()
+{
+	return n;
+}
+
+extern "C" __declspec(dllexport) LPCTSTR GetString()
+{
+	return (LPCTSTR)s;
+}
+
+extern "C" __declspec(dllexport) WCHAR* GetWchar() 
+{
+	return L"Hello dll";
+}
+'''
